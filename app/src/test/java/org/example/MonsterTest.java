d8566e99bd06bd4e6c99e80ed45a9416735b02ea
komabashiri <komamongee@git.com>
2025-07-08 12:38:58 +0900

    toStringtest

--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -4,9 +4,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonsterTest {
+
   @Test
   public void summonMonsterTest() {
     Monster classUnderTest = new Monster();
     assertEquals("スライム", classUnderTest.summonMonster(0));
+  }
+
+  @Test
+  public void toStringTest() {
+    Monster classUnderTest = new Monster();
+    assertEquals(classUnderTest.name + ":レア度[" + classUnderTest.rare + "]", classUnderTest.toString());
   }
 }
