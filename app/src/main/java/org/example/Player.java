242b19d393291f408d857a9d0131b8d924563bb3
mosunou <yoshida@github.com>
2025-07-12 02:49:03 +0900

    playerへの追加の生成

---
+++ app/src/main/java/org/example/Player.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+
+// import java.util.Random;
+
+public class Player {
+  ArrayList<Monster> hp;
+
+  Player() {
+  }
+
+}
---
+++ app/src/test/java/org/example/PlayerTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+  @Test
+  public void summonMonsterTest() {
+    Monster classUnderTest = new Monster();
+    assertEquals("スライム", classUnderTest.summonMonster(0));
+
+  }
+}
