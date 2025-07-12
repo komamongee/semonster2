7b39431fc6c1f840579cd0c387529a25c2862563
mosunou <yoshida@github.com>
2025-07-12 22:36:52 +0900

    絶賛テスト失敗中

--- app/src/main/java/org/example/Player.java
+++ app/src/main/java/org/example/Player.java
@@ -1,13 +1,40 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 // import java.util.Random;
 
 public class Player {
-  ArrayList<Monster> hp;
+  ArrayList<Monster> deck;
+  int hp;
 
   Player() {
+    Random random = new Random();
+    this.deck = new ArrayList<>();
+    this.hp = random.nextInt(5) + 5;
+    drawMonsters();
+  }
+
+  private void drawMonsters() {
+    for (int i = 0; i < 1; i++) {
+      this.deck.add(new Monster()); // ← Randomで生成
+    }
+  }
+
+  public int getHp() {
+    return this.hp;
+  }
+
+  // デッキ表示用
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (Monster m : deck) {
+      sb.append(m.toString());
+      sb.append("HP[" + this.hp + "]").append("\n");
+    }
+    return sb.toString();
   }
 
 }
--- app/src/test/java/org/example/PlayerTest.java
+++ app/src/test/java/org/example/PlayerTest.java
@@ -6,9 +6,19 @@ import static org.junit.Assert.*;
 public class PlayerTest {
 
   @Test
-  public void summonMonsterTest() {
-    Monster classUnderTest = new Monster();
-    assertEquals("スライム", classUnderTest.summonMonster(0));
+  public void getHpTest() {
+    Player classUnderTest = new Player();
+    classUnderTest.hp = 8;
+    assertEquals(8, classUnderTest.getHp());
+  }
 
+  @Test
+  public void ToStringTest() {
+    Monster classUnderTest = new Monster();
+    Player crassUnderTest = new Player();
+    classUnderTest.rare = 3;
+    crassUnderTest.hp = 4;
+    assertEquals(classUnderTest.name + ":レア度[" + classUnderTest.rare + "]HP[" + crassUnderTest.hp + "]",
+        crassUnderTest.toString());
   }
 }
